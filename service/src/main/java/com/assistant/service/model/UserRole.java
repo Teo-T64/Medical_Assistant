03c060104dc209027c8623845d22be0450c70372
Teo <teotunjic64@gmail.com>
2026-06-18 13:39:30 +0200

    initial commit

---
+++ service/src/main/java/com/assistant/service/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.assistant.service.model;
+
+public enum UserRole {
+    USER,ADMIN
+}
---
+++ service/src/test/java/com/assistant/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistant.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
