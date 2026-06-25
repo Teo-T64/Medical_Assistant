b18129aeb4c828ec26baa6ff99fe37c7763ba6e4
Teo <teotunjic64@gmail.com>
2026-06-25 22:56:18 +0200

    added

--- symptom/src/main/java/com/med/symptom/service/SymptomService.java
+++ symptom/src/main/java/com/med/symptom/service/SymptomService.java
@@ -5,6 +5,9 @@ import com.med.symptom.dto.SymptomResponse;
 import com.med.symptom.model.Symptom;
 import com.med.symptom.repository.SymptomRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,17 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SymptomService {
 
     private final SymptomRepository symptomRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public SymptomResponse trackSymptom(SymptomRequest request) {
         boolean isValidUser = userValidationService.validateUser(request.getUserId());
@@ -29,6 +39,13 @@ public class SymptomService {
                 .description(request.getDescription())
                 .build();
         Symptom savedSymptom = symptomRepository.save(symptom);
+
+        try{
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedSymptom);
+        }catch(Exception e){
+            log.error("Failed to publish to RabbitMQ: ",e);
+        }
+
         return mapToResponse(savedSymptom);
     }
 
